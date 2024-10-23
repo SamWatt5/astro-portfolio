844a540884ae25a7560ce1e54d604d32187cc63a
SamWatts5 <62394214+SamWatts5@users.noreply.github.com>
2024-10-23 10:04:04 +0100

    Update tsconfig.json and tailwind.config.mjs
    - Update tsconfig.json to preserve jsx
    - Add tailwind.config.mjs file
    
    Delete unused files and dependencies
    
    - Remove consts.ts, FormattedDate.astro, rss.xml.js, config.ts, using-mdx.mdx, BaseHead.astro, HeaderLink.astro
    - Remove unused dependencies from package.json
    
    Update astro.config.mjs and MainLayout.astro
    
    - Update astro.config.mjs to include tailwind and vue integrations
    - Update MainLayout.astro to import TheExperience.vue component

---
+++ src/components/TheExperience.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { TresCanvas } from '@tresjs/core'
+//import { TresPerspectiveCamera } from '@tresjs/core'
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
+
+const gl = {
+    clearColor: '#181C3E',
+    shadows: true,
+    alpha: false,
+    shadowMapType: BasicShadowMap,
+    outputColorSpace: SRGBColorSpace,
+    toneMappong: NoToneMapping,
+}
+</script>
+<template>
+    <div class="tres-container">
+        <TresCanvas v-bind="gl" id="app">
+            <TresPerspectiveCamera :position="[0, 0, 5]" />
+            <TresMesh>
+                <TresSphereGeometry :args="[1, 32, 32]" />
+                <TresMeshNormalMaterial />
+            </TresMesh>
+        </TresCanvas>
+    </div>
+</template>
+
+<style>
+.tres-container {
+    width: 100%;
+    height: 400px;
+}
+
+#app{
+    width: 100%;
+    height:100%;
+    background-color: red;
+}
+</style>
