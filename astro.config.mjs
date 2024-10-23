abe74f394f1860bafee622225c96986fccee20cf
SamWatts5 <62394214+SamWatts5@users.noreply.github.com>
2024-10-23 11:46:29 +0100

    how the freeeeaaakkkk does tresjs work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import vue from '@astrojs/vue';
 
+import { templateCompilerOptions } from '@tresjs/core';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  site: 'https://example.com',
+  integrations: [tailwind(), vue({
+    ...templateCompilerOptions,
+  })],
 });
--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,38 +1,28 @@
 <script lang="ts">
 import { TresCanvas } from '@tresjs/core'
-//import { TresPerspectiveCamera } from '@tresjs/core'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
 
 const gl = {
-    clearColor: '#181C3E',
-    shadows: true,
-    alpha: false,
-    shadowMapType: BasicShadowMap,
-    outputColorSpace: SRGBColorSpace,
-    toneMappong: NoToneMapping,
+  clearColor: '#181C3E',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
 }
 </script>
+
 <template>
     <div class="tres-container">
-        <TresCanvas v-bind="gl" id="app">
+        <TresCanvas v-bind="gl">
             <TresPerspectiveCamera :position="[0, 0, 5]" />
-            <TresMesh>
-                <TresSphereGeometry :args="[1, 32, 32]" />
-                <TresMeshNormalMaterial />
-            </TresMesh>
         </TresCanvas>
     </div>
 </template>
 
 <style>
-.tres-container {
-    width: 100%;
-    height: 400px;
-}
-
-#app{
-    width: 100%;
-    height:100%;
-    background-color: red;
-}
+    .tres-container {
+      width: 100%;
+      height: 400px;
+    }
 </style>
